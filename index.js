d3b649061022a85b0e7cfb44307ab50c59aaba39
stianStensli <stian.hs@hotmail.com>
2023-01-21 13:30:51 +0100

    remove console.log

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ async function main(){
     try {
         await exec.exec("/bin/bash", ["-c", "curl -fsSL https://bun.sh/install | bash"])
         core.addPath(`~/.bun/bin`)
-
-        console.log("done...")
     } catch (error) {
         core.setFailed(error.message);
     }
 }
-core.group('Downloading bun...', async () => await main())
+core.group('Downloading bun', async () => await main()).then(() => core.notice("Bun is ready!"))
