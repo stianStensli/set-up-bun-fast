6e3a79d33a7fa6dba50f23985222d484fa70ab8e
stianStensli <stian.hs@hotmail.com>
2023-01-21 11:16:54 +0100

    fix: await

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 
 
-try {
-    await exec.exec("curl -fsSL https://bun.sh/install | bash")
-    core.addPath(`${HOME}/.bun/.bun/bin/`)
-    console.log("done...")
-} catch (error) {
-    core.setFailed(error.message);
+async function main(){
+    try {
+        await exec.exec("curl -fsSL https://bun.sh/install | bash")
+        core.addPath(`${HOME}/.bun/.bun/bin/`)
+
+        console.log("done...")
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+await main()
